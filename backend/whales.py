25e4116729ca4c19719684c4d5ada5bd200c68a3
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-16 18:33:13 +0100

    Create whales.py

---
+++ backend/whales.py
@@ -0,0 +1,6 @@
+def get_whale_data():
+    return [
+        {"symbol":"BTC","type":"شراء ضخم","amount":"250 BTC","country":"USA"},
+        {"symbol":"ETH","type":"بيع ضخم","amount":"1200 ETH","country":"UAE"},
+        {"symbol":"SOL","type":"شراء متوسط","amount":"50,000 SOL","country":"Asia"}
+    ]
