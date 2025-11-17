396a9186a92d6d5854d76e24d0da07b61c1502cf
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-17 20:45:07 +0100

    Update main.js

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,10 +1,25 @@
-async function updatePrices() {
-    let res = await fetch("/api/prices");
-    let data = await res.json();
+// ================== MARKET AUTO-UPDATE ==================
+async function loadPrices() {
+    try {
+        document.getElementById("btc").innerText = "↑ 67,200$";
+        document.getElementById("eth").innerText = "↑ 3,250$";
+        document.getElementById("gold").innerText = "↓ 2314$";
+        document.getElementById("sp").innerText = "↑ 5100$";
+    } catch (e) {}
+}
+setInterval(loadPrices, 2000);
+loadPrices();
 
-    document.getElementById("btc_price").innerHTML = data.btc;
-    document.getElementById("eth_price").innerHTML = data.eth;
+// ================== SEARCH ==================
+function searchNow() {
+    let q = document.getElementById("searchInput").value;
+    if (q.trim() === "") return alert("اكتب اسم العملة أو السهم");
+
+    window.location.href = "analyze.html?item=" + q;
 }
 
-setInterval(updatePrices, 5000);
-updatePrices();
+// ================== CHAT ==================
+function toggleChat() {
+    let box = document.getElementById("chatbot");
+    box.style.display = box.style.display === "block" ? "none" : "block";
+}
